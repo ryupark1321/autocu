70101c02125ccc6b9782f5de958ce9a8f5f085b7
ryupark1321 <ryupark1321@gmail.com>
2020-09-19 14:04:15 +0000

    Making server.

--- server/Server.java
+++ server/Server.java
@@ -1,5 +1,23 @@
 package server;
 
-public class Server{
+import store.*;
+
+//Server should have a hashmap of all the brands. for reporting
+public abstract class Server{
+	public static boolean maintenance;
+
+	public static void report(){
+		return;
+	}
+
+	public static Report report(Brand b, int n_faulty){
+		return Report();
+	}
+
+	public static void main(String[] args){
+		while (maintenance == true){
+			
+		}
+	} 
 
 }
