0a0813c564f03cebfa8c159b69a2e6fd38b8035e
ryupark1321 <ryupark1321@gmail.com>
2020-10-31 10:56:59 +0000

    fixed report and server

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # autocu
-Just trying a fun project; automated convenience store
+Just trying a fun project.
+Automated Convenience Store
+-----------------------------------------------------------------
+1. Always sells freshest item first.
+2. Have a server for maintenance of all chain stores.
+
+Current Progress
+------------------------------------------------------------------
+Server and Report is put on hold for a moment.
+Need to study DB
+
 [![Run on Repl.it](https://repl.it/badge/github/ryupark1321/autocu)](https://repl.it/github/ryupark1321/autocu)
--- server/Report.java
+++ server/Report.java
@@ -9,36 +9,33 @@ class Report{
 	private int faults;
 	private Quarter quarter;
 
-	Report(Brand b, int n_faults){
-		brand = b;
-		faults = n_faults;
-		quarter = Quarter(n_faults);
-	}
-
-	Report add(int add_faults){
-		faults += n_faults;
-		return this;
-	}
-
-	Report quarterly_update(){
-		quarter = Quarter(0);
-		return this;
-	}
-
 	private class Quarter{
 		private String quarter;
 		private int quarterly_number;
 
-		Quarter(int n_faults){
+		public Quarter(int n_faults){
 			quarterly_number = n_faults;
-			month = Calendar.MONTH;
+			int month = Calendar.MONTH;
 			if(month < 4){quarter = "1st";}
 			else if(month < 7){quarter = "2nd";}
 			else if(month < 10){quarter = "3rd";}
 			else{quarter = "4th";}
 		}
-
 	}
 
+	Report(Brand b, int n_faults){
+		brand = b;
+		faults = n_faults;
+		quarter = new Quarter(n_faults);
+	}
 
+	Report add(int add_faults){
+		faults += add_faults;
+		return this;
+	}
+
+	Report quarterly_update(){
+		quarter = new Quarter(0);
+		return this;
+	}
 }
